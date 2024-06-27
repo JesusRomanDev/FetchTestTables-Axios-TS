42460b86e368c70aa09c9cb2ab9ff2f978fa7ba2
Jesus Roman <jesus_fime@outlook.es>
2024-06-27 10:36:08 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import axios from 'axios'
+import './App.css'
+import { useEffect, useState } from 'react'
+import { User } from './types';
+import UsersList from './components/UsersList';
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [showColors, setShowColors] = useState(false);
+  const [sortByCountry, setSortByCountry] = useState(false);
+  //Considerando usar el valor anterior
+  //Para mas info ir a https://react.dev/reference/react/useState#updating-state-based-on-the-previous-state
+  //En el caso de toggleColors no fue necesario usar el pending state asi como lo vemos abajo
+  // const [numbers, setNumbers] = useState(false);
+  // const toggleNumber = () => {
+  //   setNumbers(number => !number);
+  // }
+  const toggleColors = () => {
+    setShowColors(!showColors);
+  }
+  const sortCountry = () => {
+    setSortByCountry(!sortByCountry);
+  }
+
+  
+  useEffect(()=>{
+    axios.get('https://randomuser.me/api/?results=100')
+    .then(response => {
+      // console.log(response.data)
+      const {results} = response.data;
+      setUsers(results);
+      // console.log(results);
+    })
+    .catch(error => console.log(error))
+  }, [])
+
+  //Aqui en el sort, el sort nos muta el array original, por eso usamos el spread en users (osease [...users])
+  //Aparte con el sort y dandole una funcion y usando localCompare dentro de esa funcion nos va a hacer sort al country
+  //Si sortByCountry es true entonces haz el sort, de otra manera pasale los users tal cual estan
+  const sortedUsers = sortByCountry ? [...users].sort((a,b) => {
+    return a.location.country.localeCompare(b.location.country);
+  }) : users;
+  //Esto de arriba se lo pasamos al componente UsersList
+
+  return (
+    <>
+      <div>
+        <h1>Prueba Tecnica Tablas</h1>
+        <header>
+          <button onClick={toggleColors}>Colorear Filas</button>
+          <button onClick={sortCountry}>{sortByCountry ? 'No Ordenar por Pais' : 'Ordenar por Pais'}</button>
+        </header>
+        <main>
+          <UsersList showColors={showColors} users={sortedUsers} />
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UsersList.tsx
@@ -0,0 +1,48 @@
+import { User } from "../types"
+//Aqui creamos esta interface con el nombre de Props porque estamos dicendole en los parametros del componente que 
+//sera tipado siendo un User[] pero como se esta usando el destructuring es como si fueramos a destructurar tambien el type
+//por eso se creo esa interface de Props, entonces destructurando de Props vamos a tener un users 
+//Es como si tuvieramos en un componente llamado Guitarra({cualidades}), si le decimos que cualidades va a tener un type de 
+//un array de objetos osease Guitarra({cualidades} : Object[]), no funcionaria ya que dentro de Object[] no existe una propiedad
+//llamada cualidades, entonces algo asi seria 
+//interface Props {cualidades: Object[]} entonces Guitarra({cualidades} : Props)
+//Todo esto se debe a la destructuracion, sacamos users de la interface Props, entonces debe existir una propiedad llamada users
+interface Props {
+    users: User[];
+    showColors: boolean;
+}
+const UsersList = ({users, showColors}: Props) => {
+    // console.log(users);
+  return (
+    <table width='100%'>
+        <thead>
+            <tr>
+                <th>Foto</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Pais</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users.map((user, index) => {
+                // console.log(index);
+                //Asignando si el indice con modulo de 2 da 0 entonces se le aplica una clase u otra
+                const backgroundColor = index % 2 === 0 ? '#333' : '#555';
+                //Si showColors es true entonces aplicale ese color y asignaselo a la variable color
+                const color = showColors ? backgroundColor : ''
+            return (
+                <tr style={{backgroundColor: color}} key={index}>
+                    <td><img src={user.picture.thumbnail} alt="" /></td>
+                    <td>{user.name.first}</td>
+                    <td>{user.name.last}</td>
+                    <td>{user.location.country}</td>
+                    <td><button>Borrar</button></td>
+                </tr>
+            )})}
+        </tbody>
+    </table>
+  )
+}
+
+export default UsersList
